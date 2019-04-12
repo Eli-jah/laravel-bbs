56921a235bfc2c4a02bfe5dc63551077a96a863b
elijah <1198331919@qq.com>
2019-04-12 10:41:26 +0800

    feat(admin): site info management - global setting for SEO etc.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -6,6 +6,6 @@
             <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email', 'name@email.com') }}">联系我们</a></p>
     </div>
 </footer>
