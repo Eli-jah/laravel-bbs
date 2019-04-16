a7f406e2de9678c9d867dee1fe917d9e8d5f3c3b
elijah <1198331919@qq.com>
2019-04-16 14:51:46 +0800

    feat(home): set topic.index as home page

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,8 @@ Route::get('test', function () {
     return view('welcome');
 });*/
 
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 Auth::routes([
     'register' => true,
