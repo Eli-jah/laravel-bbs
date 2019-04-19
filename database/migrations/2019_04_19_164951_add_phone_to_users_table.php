ffc06e6cbb9bc3017d73e3d2015d92277b247b60
elijah <1198331919@qq.com>
2019-04-19 17:13:27 +0800

    feat(easysms): send verification code

--- database/migrations/2019_04_19_164951_add_phone_to_users_table.php
+++ database/migrations/2019_04_19_164951_add_phone_to_users_table.php
@@ -4,27 +4,31 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNotificationCountToUsersTable extends Migration
+class AddPhoneToUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
      * @return void
      */
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('notification_count')->unsigned()->default(0)->after('introduction');
+            $table->string('phone')->nullable()->unique()->after('name');
+            $table->string('email')->nullable()->change();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
      * @return void
      */
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('notification_count');
+            $table->dropColumn('phone');
+            $table->string('email')->nullable(false)->change();
         });
     }
 }
