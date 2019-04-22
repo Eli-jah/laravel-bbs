8b3b55f2e9609dce18436a4ea3453678d51e931b
elijah <1198331919@qq.com>
2019-04-22 15:11:09 +0800

    feat(verification): verification update with gregwar/captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -12,7 +12,19 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
 
         if (!app()->environment('production')) {
             $code = '1234';
@@ -22,11 +34,12 @@ class VerificationCodesController extends Controller
 
             try {
                 $result = $easySms->send($phone, [
-                    'content' => "【Larabbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信",
+                    'content' => "【LaraBBS社区】您的验证码是{$code}。如非本人操作，请忽略本短信",
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('yunpian')->getMessage();
-                return $this->response->errorInternal($message ?: '短信发送异常');
+                // return $this->response->errorInternal($message ?: '短信发送异常');
+                return $this->response->errorInternal($message ?? '短信发送异常');
             }
         }
 
@@ -35,6 +48,8 @@ class VerificationCodesController extends Controller
 
         // 缓存验证码 10分钟过期。
         Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
