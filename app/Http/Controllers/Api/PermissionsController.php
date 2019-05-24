f4ecb3bee76d9b5f10c927b188aa741117581383
elijah <1198331919@qq.com>
2019-05-24 16:48:49 +0800

    feat(permission): current user's permission index

---
+++ app/Http/Controllers/Api/PermissionsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+// use App\Http\Controllers\APi\Controller;
+use App\Transformers\PermissionsTransformer;
+use Illuminate\Http\Request;
+
+class PermissionsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $permissions = $this->user()->getAllPermissions();
+
+        return $this->response->collection($permissions, new PermissionsTransformer());
+    }
+}
